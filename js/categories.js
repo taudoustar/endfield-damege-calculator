3f304fd2fa45d92fb77962ec418597b842dfc4fc
taudoustar <taudoustar@gmail.com>
2026-03-30 12:40:28 +0800

    添加了莱万汀、埃特拉、熔铸火焰、黯色火炬、扶摇

--- js/categories.js
+++ js/categories.js
@@ -17,5 +17,6 @@ export const categories = [
 
 
   { key: "Da_Pan_MORE_SPICE!", label: "大潘独有" },
-  { key: "Rossi_Boiling_Blood", label: "洛茜独有" }
+  { key: "Rossi_Boiling_Blood", label: "洛茜独有" },
+  { key: "Laevatain_Fragments_from_the_Past", label: "莱万汀独有" }
 ];
